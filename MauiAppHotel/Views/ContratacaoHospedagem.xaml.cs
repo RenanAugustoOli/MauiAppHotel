d29d611c332bd77e022c43efd5808956b9790e09
RenanOli <renanaaaut@gmail.com>
2025-11-03 00:13:23 -0300

    Tela Adicional "Sobre"

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,9 +1,17 @@
-namespace MauiAppHotel.Views;
 
-public partial class ContratacaoHospedagem : ContentPage
+namespace MauiAppHotel.Views
 {
-	public ContratacaoHospedagem()
-	{
-		InitializeComponent();
-	}
+    public partial class ContratacaoHospedagem : ContentPage
+    {
+        public ContratacaoHospedagem()
+        {
+            InitializeComponent();
+        }
+
+        private async void Sobre_Clicked(object sender, EventArgs e)
+        {
+            
+            await Navigation.PushAsync(new Sobre());
+        }
+    }
 }
---
+++ MauiAppHotel/Views/Sobre.xaml.cs
@@ -0,0 +1,17 @@
+using Microsoft.Maui.Controls;
+
+namespace MauiAppHotel.Views
+{
+    public partial class Sobre : ContentPage
+    {
+        public Sobre()
+        {
+            InitializeComponent();
+        }
+
+        private async void Voltar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
