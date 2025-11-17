574a0052cc5cca0acad010e57bd146c9a4972ef2
renan <renan@DESKTOP-1B40QVE>
2025-11-17 08:47:19 -0300

    Check-in e Check-out

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,17 +1,44 @@
+namespace MauiAppHotel.Views;
 
-namespace MauiAppHotel.Views
+public partial class ContratacaoHospedagem : ContentPage
 {
-    public partial class ContratacaoHospedagem : ContentPage
-    {
-        public ContratacaoHospedagem()
-        {
-            InitializeComponent();
-        }
+    App PropriedadesApp;
 
-        private async void Sobre_Clicked(object sender, EventArgs e)
+    public ContratacaoHospedagem()
+    {
+        InitializeComponent();
+
+        PropriedadesApp = (App)Application.Current;
+
+        pck_quarto.ItemsSource = PropriedadesApp.lista_quartos;
+
+        dtpck_checkin.MinimumDate = DateTime.Now;
+        dtpck_checkin.MaximumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month + 1, DateTime.Now.Day);
+
+        dtpck_checkout.MinimumDate = dtpck_checkin.Date.AddDays(1);
+        dtpck_checkout.MaximumDate = dtpck_checkin.Date.AddMonths(6);
+    }
+
+    private void Button_Clicked(object sender, EventArgs e){ 
+    
+        try
         {
-            
-            await Navigation.PushAsync(new Sobre());
+            Navigation.PushAsync(new HospedagemContratada());
+
         }
+        catch (Exception ex)
+        {
+            DisplayAlertAsync("Ops", ex.Message, "OK");
+        }
+    }
+
+    private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        DatePicker elemento = sender as DatePicker;
+
+        DateTime data_selecionada_checkin = elemento.Date;
+
+        dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
+        dtpck_checkout.MaximumDate = data_selecionada_checkin.AddMonths(6);
     }
 }
